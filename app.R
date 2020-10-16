7c514d4b30343ea365d7f81775390df764bfb898
bpj2500 <72104287+bpj2500@users.noreply.github.com>
2020-10-16 16:06:59 -0400

    Add files via upload

--- app.R
+++ app.R
@@ -9,32 +9,40 @@
 
 library(shiny) 
 library(shinydashboard) 
-
+library(plotly)
 
 # Define UI for application that draws a histogram
 ui <- 
     
     dashboardPage(
-        dashboardHeader(title = "EDA of College of Data"),
+        dashboardHeader(title = "EDA of College Data"),
         dashboardSidebar(
             sidebarUserPanel("Author : Brian Perez"), 
             
             sidebarMenu( 
                 menuItem("General Trends in the USA", 
                          tabName = "intro", icon = icon("book-reader"),
-                         menuSubItem("Demographics 2014", tabName = "Subtab 1"))
+                         menuSubItem("Demographics 2014", tabName = "subitem1"), 
+                         menuSubItem("Countrywide Yearly Trends", tabName = "subitem2"))
                 )
             
         ),
         
         
 #########        
-        dashboardBody(
-            tabItem(tabName = "Demographics 2014", h2("General Trends in the United States"), 
-                fluidRow( 
-                    box(plotOutput("plot1"), width = 4), 
-                    box(plotOutput("plot2"), width = 6)
-                    )
+        dashboardBody( 
+            tabItems(      
+                tabItem(tabName = "subitem1", h2("General Trends in the United States"), 
+                        fluidRow( 
+                            box(plotlyOutput("plot1"), width = 4), 
+                            box(plotlyOutput("plot2"), width = 6)
+                        )
+                ),  
+                tabItem(tabName = "subitem2", h2("Trends in the United States"), 
+                        fluidRow( 
+                            box(plotlyOutput("plot3"), width = 4), 
+                            box(plotlyOutput("plot4"), width = 6)
+                            ))
                 )
         )
 )
@@ -54,7 +62,7 @@ ui <-
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-    output$plot1 <- renderPlot( 
+    output$plot1 <- renderPlotly( 
         college_data %>% 
             filter(category != "Total Minority" & category != "Unknown") %>% 
             group_by(type, category) %>%
@@ -69,7 +77,7 @@ server <- function(input, output) {
             theme_bw() +
             theme(axis.text = element_text(angle =90, hjust = 1)) 
         )
-    output$plot2 <- renderPlot( 
+    output$plot2 <- renderPlotly( 
         college_data %>% 
             filter(category != "Total Minority" & category != "Unknown") %>% 
             group_by(type, category) %>%
@@ -91,12 +99,32 @@ server <- function(input, output) {
             geom_col(position = "dodge") + 
             coord_flip() + 
             theme_bw()
-        
-        
-        
-        
-        
         )
+    output$plot3 <- renderPlotly(  
+        college_data %>% 
+            group_by(type, year) %>% 
+            summarise(full_tuition = median(total_price)) %>% 
+            arrange(desc(full_tuition)) %>% 
+            ggplot(aes(x = year, y = full_tuition)) + 
+            geom_point(aes(color = type)) + 
+            geom_line(aes(color = type)) +
+            theme(axis.text = element_text(angle =0, hjust = 1)) + 
+            coord_cartesian(xlim = c(2010,2018)) + 
+            theme_bw()
+        )
+    output$plot4 <- renderPlotly( 
+        college_data %>% 
+            group_by(type, year) %>% 
+            summarise(student_cost = median(net_cost)) %>% 
+            arrange(desc(student_cost)) %>% 
+            ggplot(aes(x = year, y = student_cost)) + 
+            geom_point(aes(color = type)) + 
+            geom_line(aes(color = type)) +
+            theme(axis.text = element_text(angle =0, hjust = 1)) + 
+            coord_cartesian(xlim = c(2010,2018)) + 
+            theme_bw()
+        )
+    
 }
 
     
